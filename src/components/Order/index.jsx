f93978fb741a3981e67f2233fb6b0511b3ef4bc4
Barbora Kalvodová <kalvodova.bara@seznam.cz>
2025-07-02 00:27:43 +0200

    Show order details on order page

---
+++ src/components/Order/index.jsx
@@ -0,0 +1,33 @@
+import { OrderItem } from "../OrderItem"
+
+export const Order = ({items}) => {
+    console.log(items);
+    
+    return (
+        <>
+            {items.length === 0 ? (
+                <p className="empty-order">Zatím nemáte nic objednáno</p>
+            ) : (
+                items.map((item) => (
+                <OrderItem 
+                    key={item.id}
+                    name={item.name}
+                    image={`http://localhost:4000${item.image}`}
+                />
+            )))}
+        </>
+    )
+}
+
+/*
+        <>
+            {items.map((item) => (
+                <OrderItem 
+                    key={item.id}
+                    name={item.name}
+                    image={`http://localhost:4000${item.image}`}
+                />
+            ))}
+        </>
+
+        */
